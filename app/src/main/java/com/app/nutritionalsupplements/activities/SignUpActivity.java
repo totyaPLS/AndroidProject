49d8161329f285f8aba7c2760a9bd00e36a2ea76
Ákos-PC <totyavidpulse@gmail.com>
2023-04-07 00:49:56 +0200

    sign up activity logs

--- app/src/main/java/com/app/nutritionalsupplements/activities/SignUpActivity.java
+++ app/src/main/java/com/app/nutritionalsupplements/activities/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.app.nutritionalsupplements.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,6 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     private static final String PREF_KEY = MainActivity.PACKAGE_NAME;
 
     EditText username;
+    EditText email;
     EditText password;
     EditText passwordAgain;
     EditText postalAddress;
@@ -27,10 +29,31 @@ public class SignUpActivity extends AppCompatActivity {
         if (secretKey != 99) {
             finish();
         }
+
+        initializeData();
+    }
+
+    private void initializeData() {
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        passwordAgain = findViewById(R.id.password_again);
+        postalAddress = findViewById(R.id.postal_address);
     }
 
     public void register(View view) {
+        String usernameStr = username.getText().toString();
+        String emailStr = email.getText().toString();
+        String passwordStr = password.getText().toString();
+        String passwordAgainStr = passwordAgain.getText().toString();
+        String addressStr = postalAddress.getText().toString();
 
+        if (!passwordStr.equals(passwordAgainStr)) {
+            Log.e(LOG_TAG, "The passwords do not match!");
+            return;
+        }
+
+        Log.i(LOG_TAG, usernameStr + "; " + emailStr + "; " + passwordStr + "; " + passwordAgainStr + "; " + addressStr);
     }
 
     public void back(View view) {
